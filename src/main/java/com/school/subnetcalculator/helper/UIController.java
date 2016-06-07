e204d03f9fb7ec1e1f998855ccab4b92f5cc0c22
ThiesJac <thies.jacobsen94@gmail.com>
2016-06-07 10:29:34 +0200

    added SF Fonts to label, list, textfield, etc.

--- src/main/java/com/school/subnetcalculator/helper/UIController.java
+++ src/main/java/com/school/subnetcalculator/helper/UIController.java
@@ -7,13 +7,30 @@ public class UIController {
 	
 	public UIController() {
 		setLookAndFeel();
+		setFont();
 	}
-	
+
 	private void setLookAndFeel() {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException | InstantiationException
 				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setFont() {
+		try {
+			InputStream is = SubnetCalculatorFrame.class
+					.getResourceAsStream("System San Francisco Display Regular.ttf");
+			Font createFont = Font.createFont(Font.TRUETYPE_FONT, is);
+			UIManager.put("Button.font", createFont.deriveFont(16.0f));
+			UIManager.put("Label.font", createFont.deriveFont(16.0f));
+			UIManager.put("TextField.font", createFont.deriveFont(16.0f));
+			UIManager.put("List.font", createFont.deriveFont(16.0f));
+			UIManager.put("TextArea.font", createFont.deriveFont(16.0f));
+			UIManager.put("TabbedPane.font", createFont.deriveFont(16.0f));
+		} catch (FontFormatException | IOException e) {
 			e.printStackTrace();
 		}
 	}
