2b35474d547723a8bc0c04957a17fde97b96f1dc
Jonah Möller <jonah.moeller@outlook.com>
2016-06-08 12:02:04 +0200

    added feature branch and constructor placeholder

--- src/main/java/com/school/subnetcalculator/model/Network.java
+++ src/main/java/com/school/subnetcalculator/model/Network.java
@@ -1,61 +1,60 @@
 package com.school.subnetcalculator.model;
 
+import com.googlecode.ipv6.IPv6Address;
 import com.googlecode.ipv6.IPv6Network;
+import com.googlecode.ipv6.IPv6NetworkMask;
+
 import java.util.Collections;
 import java.util.List;
 
-public class Network
-{
+public class Network {
     // TODO: missing IPv4Network thing
     private IPv6Network ipv6Network;
     private List<Subnet> subnetList;
 
 
-    public Network()
-    {
+    public Network(IPv6Address networkAddress, IPv6NetworkMask subnetMask) {
+        this.ipv6Network = IPv6Network.fromAddressAndMask(networkAddress, subnetMask);
 
     }
 
+    public Network() {
+        // TODO: repeat the above for IPv4
+    }
 
-    public List<Subnet> getSubnetList()
-    {
+
+    public List<Subnet> getSubnetList() {
         return subnetList;
     }
 
 
-    public void setSubnetList(List<Subnet> subnetList)
-    {
+    public void setSubnetList(List<Subnet> subnetList) {
         this.subnetList = subnetList;
     }
 
 
-    public void addSubnet(Subnet subnet)
-    {
+    public void addSubnet(Subnet subnet) {
         this.subnetList.add(subnet);
     }
 
 
-    public void removeSubnetFromList(Subnet subnet)
-    {
+    public void removeSubnetFromList(Subnet subnet) {
         this.subnetList.remove(subnet);
     }
 
 
-    public List<Subnet> getCalculatedSubnetList(int subnetCount)
-    {
+    public List<Subnet> getCalculatedSubnetList(int subnetCount) {
         // TODO: Of some host count.
         return Collections.emptyList();
     }
 
 
-    public IPv6Network getIpv6Network()
-    {
+    public IPv6Network getIpv6Network() {
         return ipv6Network;
     }
 
 
-    public void setIpv6Network(IPv6Network ipv6Network)
-    {
+    public void setIpv6Network(IPv6Network ipv6Network) {
         this.ipv6Network = ipv6Network;
     }
 }
