ea0a979f30675806f7851f0462d45f4d9a7ad192
Jonah Möller <jonah.moeller@outlook.com>
2016-06-17 10:25:38 +0200

    fixed stuff

--- src/main/java/com/school/subnetcalculator/helper/DocumentParser.java
+++ src/main/java/com/school/subnetcalculator/helper/DocumentParser.java
@@ -2,18 +2,11 @@ package com.school.subnetcalculator.helper;
 
 import com.school.subnetcalculator.mapper.NetworkMapper;
 import com.school.subnetcalculator.model.Network;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
+import org.json.JSONObject;
+
+import java.io.*;
 import java.util.Collections;
 import java.util.List;
-import org.json.JSONObject;
 
 public class DocumentParser
 {
@@ -39,19 +32,10 @@ public class DocumentParser
         JSONObject jsonObject = NetworkMapper.makeJsonObjectFromNetworkList(networkList);
 
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-            new FileOutputStream(exportFileName), "utf-8")))
+            new FileOutputStream(exportFileName, false), "utf-8")))
         {
             writer.write(jsonObject.toString());
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            e.printStackTrace();
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
+        } catch (IOException e)
         {
             e.printStackTrace();
         }
